d13cc5aca125490e77e16ed95c5f206aa1841812
+ <+>
2023-09-18 23:50:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
 
-// Write your code here
+let num2 = 2; 
+
+let multiply = num1 * num2;
+
+let num3 = 22;
+
+let num4 = 9;
+
+let mod = num3 % num4;
+
+let numberArray = [10, 5, 20, 15];
+
+let max = Math.max(...numberArray);
+
+let random = Math.floor(Math.random() * 100) +1;
